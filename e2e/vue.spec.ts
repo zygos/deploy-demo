f207e7d5603a54a9667bc48af67f74be4df6dc30
zygos <zaibasxs@gmail.com>
2024-03-20 18:41:53 +0200

    ci: test out

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('./');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
+  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!!');
 })
 
 test('about page', async ({ page }) => {
